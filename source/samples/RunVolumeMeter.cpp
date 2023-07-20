833336c8c6ec9d5772fd7266e77a45fd0bb94f2c
Jasper <jasper.precht@web.de>
2023-07-20 13:58:53 +0200

    Updating Demo
    - adding LED functions
    - volumeter running

--- source/samples/RunVolumeMeter.cpp
+++ source/samples/RunVolumeMeter.cpp
@@ -2,93 +2,109 @@
 #include "RunVolumeMeter.h"
 #include "MicroBit.h"
 #include "Utils.h"
-// #include "neopixels.h"
+#include "LevelDetector.h"
+#include "LevelDetectorSPL.h"
+
+#define MICROPHONE_MIN 52.0f
+#define MICROPHONE_MAX 120.0f
 
 extern MicroBit uBit;
 
-const int threshold = 519;
-
-static int read()
+long map(long x, long in_min, long in_max, long out_min, long out_max)
 {
-    int mic = 0;
-    const int n = 30;
-
-    uBit.io.runmic.setDigitalValue(1);
-    uBit.io.runmic.setHighDrive(true);
-
-    for (uint8_t i = 0; i < n; i++) {
-        // assign new Pin of microphone?
-        const int v = uBit.io.microphone.getAnalogValue();
-        if (v > mic) mic = v;
-    }
-
-    if (mic < threshold) {
-        return 0;
-    }
-
-    // mic 512...900 ~> 0...5
-    const int gauge = static_cast<const int>((log2(mic - threshold + 1) * 5) / 8);
-
-    // uBit.serial.printf("m:%d -> g:%d\n", mic, gauge);
-
-    if (gauge < 0) {
-        return 0;
-    }
-
-    if (gauge > 5) {
-        return 5;
-    }
-
-    return gauge;
+    return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
 }
+
+int soundLevel()
+{
+    LevelDetectorSPL *level = uBit.audio.levelSPL;
+    if (NULL == level)
+        return 0;
+    const int micValue = level->getValue();
+    const int scaled = max(MICROPHONE_MIN, min(micValue, MICROPHONE_MAX)) - MICROPHONE_MIN;
+    return min(0xff, scaled * 0xff / (MICROPHONE_MAX - MICROPHONE_MIN));
+}
+
+// static int read()
+// {
+//     // int mic = 0;
+//     // const int n = 30;
+
+//     uBit.audio.activateMic();
+
+//     // for (uint8_t i = 0; i < n; i++)
+//     // {
+//     //     mic = soundLevel();
+//     // }
+
+//     // mapping values in range of LEDs
+//     const int gauge = map(soundLevel(), 0, 100, 0, 5);
+
+//     if (gauge < 0)
+//     {
+//         return 0;
+//     }
+
+//     if (gauge > 5)
+//     {
+//         return 5;
+//     }
+
+//     return gauge;
+// }
 
 void volumemeter_run()
 {
-    uBit.messageBus.listen(
-        MICROBIT_ID_BUTTON_AB,
-        MICROBIT_BUTTON_EVT_CLICK,
-        leaveHandler);
+    uBit.audio.activateMic();
+    uBit.messageBus.listen(MICROBIT_ID_BUTTON_AB, MICROBIT_BUTTON_EVT_CLICK, leaveHandler);
 
     int gauges[5] = {};
 
     leave = false;
-    while (!leave) {
+    while (!leave)
+    {
 
         // shift values
-        for (uint8_t i = 0; i < (5-1); i++) {
-            gauges[i] = gauges[i+1];
+        for (uint8_t i = 0; i < (5 - 1); i++)
+        {
+            gauges[i] = gauges[i + 1];
         }
 
-        gauges[4] = read();
+        gauges[4] = map(soundLevel(), 0, 100, 0, 5);
 
-        for (uint16_t x = 0; x < 5; x++) {
+        for (uint16_t x = 0; x < 5; x++)
+        {
             const int gauge = gauges[x];
-            for (uint16_t y = 0; y < 5; y++) {
+            for (uint16_t y = 0; y < 5; y++)
+            {
                 const uint8_t t = static_cast<uint8_t>(gauge > y ? 255 : 0);
-                uBit.display.image.setPixelValue(x, 4-y, t);
+                uBit.display.image.setPixelValue(x, 4 - y, t);
             }
         }
 
         int sum = 0;
-        for (uint8_t i = 0; i < 5; i++) {
+        for (uint8_t i = 0; i < 5; i++)
+        {
             sum += gauges[i];
         }
 
-        if (sum > 5*2.5) {
-            // neopixels
-           // uBit.rgb.setColour(0xFF, 0x00, 0x00, 0x00);
-        } else {
-            //neopixels
-          //  uBit.rgb.setColour(0x00, 0xFF, 0x00, 0x00);
+        if (sum > 5 * 3)
+        {
+            LEDTrafficLight(2);
+        }
+        else if (sum > 5 * 1.5)
+        {
+            LEDTrafficLight(1);
+        }
+        else
+        {
+            LEDTrafficLight(0);
         }
 
         uBit.sleep(100);
     }
 
-   // uBit.rgb.off();
+    // uBit.rgb.off();
 
-    uBit.messageBus.ignore(
-        MICROBIT_ID_BUTTON_AB,
-        MICROBIT_BUTTON_EVT_CLICK,
-        leaveHandler);
+    uBit.messageBus.ignore(MICROBIT_ID_BUTTON_AB, MICROBIT_BUTTON_EVT_CLICK, leaveHandler);
 }
