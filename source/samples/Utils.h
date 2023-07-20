d21deeabbe38d0b5b7dbd20a5510d0daa523a300
Jasper <jasper.precht@web.de>
2023-07-20 13:58:53 +0200

    Updating Demo
    - adding LED functions
    - volumeter running

--- source/samples/Utils.h
+++ source/samples/Utils.h
@@ -22,4 +22,18 @@ void blinkImageUntilEvent(
     const int pos2 = 0,
     const uint16_t delay = 200);
 
+void shakeEvent(MicroBitEvent);
+
+void LEDTrafficLight(int color);
+
+void disableLEDs();
+
+void startLEDs();
+
+void turnLEDon(int start_index, int end_index);
+
+void LEDcounter(int number);
+
+
+
 #endif // UTILS_H
