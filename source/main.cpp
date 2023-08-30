9a7ba82188582f2caa3d240e37fc3fb3d7b65430
fabianhugo <huwg@mailbox.org>
2023-08-30 11:31:16 +0200

    shows 3 colors on 3 leds

--- source/main.cpp
+++ source/main.cpp
@@ -6,6 +6,22 @@ MicroBit uBit;
 int main()
 {
     uBit.init();
+    int length = 3;
+    // Brightness of pixels
+    int brightness = 32;
+    // Create a buffer to hold pixel data of the appropriate size
+    ManagedBuffer b(length*3);
+    b.fill(0);
+    b[0]=brightness; //turns 1st LED green
+    b[1]=0;
+    b[2]=0;
+    b[3]=0;
+    b[4]=brightness; //turns 2nd LED red
+    b[5]=0;
+    b[6]=0;
+    b[7]=0;
+    b[8]=brightness; //turns 3rd LED blue
+    neopixel_send_buffer(uBit.io.RGB, b);
 
-    out_of_box_experience();
+    // out_of_box_experience();
 }
