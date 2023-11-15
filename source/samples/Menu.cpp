2e7fc348ccb57159a76d104d5ae4f97bab4c3e13
fabianhugo <huwg@mailbox.org>
2023-11-15 16:41:33 +0100

    set reset to test to resitive touch mode

--- source/samples/Menu.cpp
+++ source/samples/Menu.cpp
@@ -60,7 +60,7 @@ menustate_t menuWaitForChoice(menustate_t start)
                 return state;
             }
             // reset to test mode by touching all 4 edges
-            if ((uBit.io.P0.isTouched(TouchMode::Capacitative)==1) && (uBit.io.P1.isTouched(TouchMode::Capacitative)==1) && (uBit.io.P2.isTouched(TouchMode::Capacitative)==1)&& (uBit.io.P3.isTouched(TouchMode::Capacitative)==1)){
+            if (uBit.io.P0.isTouched() && uBit.io.P1.isTouched() && uBit.io.P2.isTouched() && uBit.io.P3.isTouched()){
             uBit.storage.remove("counter");   
             uBit.display.print("TestMode");
             uBit.reset(); 
